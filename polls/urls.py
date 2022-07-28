332f909d025e31e4a0dd4e2b5fb1e7c1a32d1564
adegtun <adegtun@yahoo.com>
2022-07-28 12:33:12 +0100

    making changes to the url specification

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,7 @@ from unicodedata import name
 from . import views
 from django.urls import path
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
