a06765bf14b6752019d5e2577d96d076e7997a25
adegtun <adegtun@yahoo.com>
2022-07-28 11:40:14 +0100

    initial commit

---
+++ polls/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello world")
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You are looking at results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
